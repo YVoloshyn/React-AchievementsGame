d9f3563cbf0f794fa0dcc72cb14a5115e0f9854d
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2018-07-13 08:32:31 +0300

    add new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
 
+class AchievementsDisplay extends Component{
+    render() {
+        return (
+            <h2>Here will be the text about selected Achievement!</h2>
+        )
+    }
+}
+
 class App extends Component {
     render() {
         return (
-            <header><h1>This is a Unicorn Game</h1>h1></header>
-
-
+            <div className="App">
+                <AchievementsDisplay />
+            </div>
         );
     }
 }
