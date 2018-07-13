1edd41b23b5b22e7b57b4f76f4f46edc179f4bca
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2018-07-13 09:07:43 +0300

    add logic to display another sentance on load

--- src/App.js
+++ src/App.js
@@ -16,23 +16,59 @@ class AchievementsDisplay extends Component{
     }
 }
 
-class App extends Component {
+class AchievementNotSelected extends Component {
     render() {
         return (
-            <div className="App">
-                <AchievementsDisplay id={2}/>
-                {Achievements.map((place, id) => (
-                    <button
-                        key={id}
-                        onClick={() => {
-                            console.log('Clicked index '+id);
-                        }}
-                    >
-                        {place.name}
-                    </button>
-                ))}
-            </div>
-        );
+            <h2>Please, select any Achievement to read more!</h2>
+        )
+    }
+}
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            activeAchievement: null,
+        };
+    }
+    render() {
+        const activeAchievement = this.state.activeAchievement;
+        if (activeAchievement !== null) {
+            return (
+                <div className="App">
+                    <AchievementsDisplay
+                        key={activeAchievement}
+                        id={Achievements[activeAchievement].id}
+                    />
+                    {Achievements.map((place, id) => (
+                        <button
+                            key={id}
+                            onClick={() => {
+                                this.setState({activeAchievement: id});
+                            }}
+                        >
+                            {place.name}
+                        </button>
+                    ))}
+                </div>
+            );
+        }else {
+            return (
+                <div className="NotSelected">
+                    <AchievementNotSelected />
+                    {Achievements.map((place, id) => (
+                        <button
+                            key={id}
+                            onClick={() => {
+                                this.setState({activeAchievement: id});
+                            }}
+                        >
+                            {place.name}
+                        </button>
+                    ))}
+                </div>
+            );
+        }
     }
 }
 
