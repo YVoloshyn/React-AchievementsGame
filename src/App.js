ca109b0d5c077b96b4c7b489586111ba3fa5d2df
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2018-07-13 08:22:00 +0300

    created template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <header><h1>This is a Unicorn Game</h1>h1></header>
+
+
+        );
+    }
+}
+
+export default App;
