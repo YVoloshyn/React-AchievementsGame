3cf78ed181316799bed7027aeaffa7e4a107c88f
Yaroslav Voloshyn <jarik.voloshyn@gmail.com>
2018-07-13 08:49:24 +0300

    add buttons to select achievement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 
+const Achievements = [
+    { name: "Bomb Magnet", id: "1" },
+    { name: "Perfectionist", id: "2" },
+    { name: "Night Shift", id: "3" },
+    { name: "Connected Ninja", id: "4" }
+];
+
 class AchievementsDisplay extends Component{
     render() {
         return (
-            <h2>Here will be the text about selected Achievement!</h2>
+            <h2>Displaying Achievement description for id {this.props.id}!</h2>
         )
     }
 }
@@ -13,7 +20,17 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <AchievementsDisplay />
+                <AchievementsDisplay id={2}/>
+                {Achievements.map((place, id) => (
+                    <button
+                        key={id}
+                        onClick={() => {
+                            console.log('Clicked index '+id);
+                        }}
+                    >
+                        {place.name}
+                    </button>
+                ))}
             </div>
         );
     }
